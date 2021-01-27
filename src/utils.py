cc3f73bf55096cdde0ae63851a8b3cec43279f49
Tanveer81 <tanveer.hannan@profil.lmu.de>
2021-01-27 14:36:27 +0100

    data generator bug fix

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from numpy.random import randint
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 
@@ -69,7 +69,6 @@ def evaluate_model(y_true, y_pred):
 def save(metrics_test):
     pass
 
-
 class DataGenerator():
 
   def __init__(self, datapath, imputed, preprocessed):
@@ -102,7 +101,7 @@ class DataGenerator():
 
       ordinal_enc_dict[col_name] = OrdinalEncoder()
 
-      col = bank_data[col_name]
+      col = self.bank_data[col_name]
       list_of_indices_not_na = []
       for i, value in enumerate(col):
         if value != "unknown":
@@ -111,9 +110,7 @@ class DataGenerator():
       col.replace({"unknown": np.nan}, inplace=True)
       col_notna = col[col.notna()]
       reshaped_colvalues = col_notna.values.reshape(-1, 1)
-
       encoded_vals = ordinal_enc_dict[col_name].fit_transform(reshaped_colvalues)
-
       for i, value in enumerate(list_of_indices_not_na):
         encoded_bank_data[col_name][value] = encoded_vals[i]
 
@@ -130,6 +127,9 @@ class DataGenerator():
     for col in self.cat_features:
       reshaped_col = imputed_bank_data[col].values.reshape(-1, 1)
       imputed_bank_data[col] = ordinal_enc_dict[col].inverse_transform(reshaped_col)
+
+    # save a copy of the imputed data for future use. 
+    imputed_bank_data.to_csv("bank_data_mice.csv", sep=";", index=False)
     
     return imputed_bank_data
 
@@ -142,8 +142,8 @@ class DataGenerator():
 
   def bin_duration(self):
 
-    duration_bin_labels = ['less_than_200sec', '200sec_400sec', '400sec_800sec', 
-                    'more_than_800sec']
+    duration_bin_labels = ['less than 200sec', '200sec - 400sec', '400sec - 800sec', 
+                    'more than 800sec']
     dur_bin_size = [-1, 200, 400, 800, 200000]
 
     # remove outliers
@@ -162,8 +162,8 @@ class DataGenerator():
     self.category_map = {}
     for i, feature in enumerate(self.cat_features + self.target):
       if feature == "binned_duration":
-        self.category_map[i] = ["less_than_200sec", "200_400sec", "400-800sec",
-                                "more_than_800sec"]
+        self.category_map[i] = ['200sec - 400sec', '400sec - 800sec',
+                                'less than 200sec', 'more than 800sec']
         continue
       self.category_map[i] = sorted(self.bank_data[feature].unique())
 
@@ -204,14 +204,20 @@ class DataGenerator():
     self.bank_data.loc[self.bank_data['education'] == 'basic.9y','education'] = 'basic'
     self.bank_data.loc[self.bank_data['education'] == 'basic.6y','education'] = 'basic'
     self.bank_data.loc[self.bank_data['education'] == 'basic.4y','education'] = 'basic'
-    self.bank_data['education'] = self.bank_data['education'].str.replace('.','')
-    self.bank_data['job'] = self.bank_data['job'].str.replace('.','')
+    self.bank_data['education'] = self.bank_data['education'].str.replace('.',' ')
+    self.bank_data['job'] = self.bank_data['job'].str.replace('.',' ')
 
     # Step 6: One-Hot Encode Categorical Features
     
-    # self.cat_vars_ohe = ord_to_ohe(np.array(self.bank_data), self.cat_vars_ord)[1]
-
     self.define_cat_maps()
+
+    self.pre_one_hot_encoded_data = self.bank_data.copy()
+    self.pre_one_hot_encoded_data.drop(columns=["y"], inplace=True)
+    self.pre_one_hot_encoded_data = self.pre_one_hot_encoded_data[[c 
+                                                                   for c in self.pre_one_hot_encoded_data
+                                                                   if c not in self.num_features] 
+                                                                  + self.num_features]
+    self.pre_one_hot_encoded_data.reset_index(drop=True, inplace=True)
 
     self.bank_data = pd.get_dummies(self.bank_data, columns=self.cat_features)
     self.bank_data = pd.get_dummies(self.bank_data, columns=self.target, drop_first=True)
@@ -236,19 +242,50 @@ class DataGenerator():
     x_test = x_test[[c for c in x_train if c not in self.num_features]
                       + self.num_features]
     
+    self.column_names = x_train.columns
+
     return x_train, x_test, y_train, y_test
 
 
-  def reverse_cat_features(data):
+  def reverse_cat_features(self, data):
 
-    return data
+    data = np.array(data).reshape((1, len(self.column_names)))
+    X_orig_ord = ohe_to_ord(data, self.cat_vars_ohe)[0]
+    cat_vars = []
+    for i, (_, v) in enumerate(self.category_map.items()):
+
+      cat_orig = v[int(X_orig_ord[0, i])]
+      cat_vars.append(cat_orig)
+
+    return cat_vars[:-1]
 
 
   def rev_transform(self, data, rev_num_features=True, rev_cat_features=True):
+    '''
+    accepts one dataframe row containing one hot encoded values. Shape (1, 55)
+    '''
 
-    if rev_num_features:
+    if rev_cat_features and rev_num_features:
+      feature_values = self.reverse_cat_features(data) + list(self.scaler.inverse_transform(data[self.num_features]))
+      data = pd.DataFrame([feature_values], columns=self.cat_features + self.num_features)
+
+    elif rev_num_features:
       data[self.num_features] = self.scaler.inverse_transform(data[self.num_features])
-    if rev_cat_features:
-      data[self.cat_features] = self.reverse_cat_features(data[self.cat_features])
     
+    else:
+      feature_values = self.rev_cat_features(data) + list(data[self.num_features])
+      data = pd.DataFrame([feature_values], columns=self.cat_features + self.num_features)
     return data
+  
+
+  def transform(self, instance):
+
+    self.pre_one_hot_encoded_data.loc[len(self.pre_one_hot_encoded_data)] = instance
+    encoded_data = pd.get_dummies(
+        self.pre_one_hot_encoded_data, columns=self.cat_features)
+    encoded_data[self.num_features] = self.scaler.transform(encoded_data[self.num_features])
+    encoded_data = encoded_data[[c for c in encoded_data 
+                                 if c not in self.num_features] 
+                                + self.num_features]
+
+    return encoded_data.loc[len(encoded_data)-1]
